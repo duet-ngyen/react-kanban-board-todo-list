4607b284d59b70965ed198fc2233060d8cac2c6d
Duyet Nguyen <duyet.nguyen@outlook.com>
2016-05-24 15:54:47 +0700

    reformat struct branch master

--- app/App.js
+++ app/App.js
@@ -1,3 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
 
+class App extends Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        );
+    }
+}
+
+render(<App />, document.getElementById('root'));
